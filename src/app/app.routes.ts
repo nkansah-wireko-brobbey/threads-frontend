7e1206d17834c4b2a559e01242e2671990f01f6e
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-10-31 18:06:55 -0500

    feat: route update to include layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/default/default.component').then(
+        (l) => l.DefaultComponent
+      ),
+    children: [
+      {
         path: '',
-        loadComponent: ()=> import('./pages/home/home.component').then((p)=> p.HomeComponent),
-    }
+        loadComponent: () =>
+          import('./pages/home/home.component').then((p) => p.HomeComponent),
+      },
+    ],
+  },
 ];
