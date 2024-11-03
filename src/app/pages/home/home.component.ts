5d47b293454bdd2263e6842d122c202d5d1df2ee
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-11-03 13:44:42 -0600

    feat: comment integration to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommentComponent } from '../../components/comment/comment.component';
+import { CommentService } from '../../services/comment.service';
+import { Comment } from '../../interfaces/comment';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommentComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  commentService = inject(CommentService);
 
+  comments = signal<Comment[]>([]);
+
+  ngOnInit(): void {
+    this.getComments()
+  }
+
+  getComments() {
+    this.commentService.getComments().subscribe((comments) => {
+      this.comments.set(comments);
+    });
+  }
 }
