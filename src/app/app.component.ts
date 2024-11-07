606e30d2f9cd177afbcf349ffa1cc2f127060a1e
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-11-06 23:07:53 -0600

    feat: added user creation on start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './services/user.service';
+import { StorageService } from './services/storage.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  userService = inject(UserService);
+  storageService = inject(StorageService);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const user = this.storageService.getUserFromStorage();
+
+    if (!user) {
+      console.log('No User');
+      const newUsername = 'user_' + Math.random() * 7000 + 1000;
+
+      this.userService.createUser(newUsername).subscribe((data) => {
+        console.log(data);
+      });
+    }
+  }
+
   title = 'threads-frontend';
 }
